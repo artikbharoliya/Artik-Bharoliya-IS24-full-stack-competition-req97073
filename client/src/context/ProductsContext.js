e927e55a5fed38edef32a2d7801f80a93d55e6c9
Artik Bharoliya <artikbharoliya67@gmail.com>
2023-03-30 20:42:15 -0700

    added error handling

--- client/src/context/ProductsContext.js
+++ client/src/context/ProductsContext.js
@@ -1,3 +1,4 @@
+import { Alert, Snackbar } from "@mui/material";
 import { createContext, useEffect, useState } from "react";
 
 export const ProductsContext = createContext({});
@@ -7,9 +8,13 @@ const api_url = 'api/products';
 const ProductsContextProvider = (props) => {
   const [products, setProducts] = useState({});
 
+  const [error, setError] = useState(false);
   // fetching the data to initialize the context.
   const fetchProducts = async () => {
     const response = await fetch(api_url);
+    if (!response.ok) {
+      setError(true);
+    }
     const products = await response.json();
     setProducts(products);
   }
@@ -19,6 +24,11 @@ const ProductsContextProvider = (props) => {
 
   return (
     <ProductsContext.Provider value={[products, setProducts]}>
+      <Snackbar open={error} autoHideDuration={6000} >
+        <Alert severity="error" sx={{ width: '100%' }}>
+          Can't fetch the products!
+        </Alert>
+      </Snackbar>
       {props.children}
     </ProductsContext.Provider>
   );
